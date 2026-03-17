721b3084bc3aa775cb6e5ef4883b5f4b2c2fe288
bamboo-jzy <bamboomail_j@163.com>
2026-03-17 10:31:04 +0800

    update

--- cases/test_par.py
+++ cases/test_par.py
@@ -1,8 +1,6 @@
-from common.parametrize import parametrize
+from playwright.sync_api import Page
 
+def test_has_title(page: Page):
+    page.goto("https://playwright.nodejs.cn/")
 
-@parametrize("data/test.xlsx", dtype={"n1": str})
-def test_pardd(n1, n2, n3):
-    assert type(n3) is float
-    assert type(n2) is int
-    assert type(n1) is str
+    assert False
